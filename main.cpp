0a55816a63a5ebca1c91542bed13630406794c34
Gregwar <g.passault@gmail.com>
2017-06-02 14:23:17 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -3,24 +3,29 @@
 #include <cmath>
 #include <RhIO.hpp>
 
-class A
+class MyClass
 {
     public:
-        A() : bind("demo")
+        MyClass() : bind("demo")
         {
             t = 0;
 
+            // Sine amplitude parameter, can be persited if you type "save"
             bind.bindNew("a", a, RhIO::Bind::PullOnly)
                 ->defaultValue(1)->comment("Sine amplitude")->persisted(true);
 
+            // Example of boolean parameter
             bind.bindNew("shift", shift, RhIO::Bind::PullOnly)
                 ->defaultValue(false);
 
+            // Two parameters
             bind.bindNew("x", x, RhIO::Bind::PushOnly);
             bind.bindNew("y", y, RhIO::Bind::PushOnly);
 
+            // A test command
             bind.bindFunc("test", "test command", &A::myCmd, *this);
 
+            // A stream to debug
             bind.newStream("debug", "Debugging");
         }
 
@@ -35,17 +40,21 @@ class A
 
         void tick()
         {
+            // Updating variables from RhIO
             bind.pull();
 
+            // Doing some calculus
             x = sin(t)*a;
             if (shift) x += 10;
             y = cos(t)*a;
-            printf("%f\n", x);
 
+            // Sending variables to RhIO
             bind.push();
+
             t += 0.001;
+
+            // Sending data to the debug node, can be monitored with cat debug
             bind.out("debug") << "Calling usleep! t=" << t << std::endl;
-            usleep(1000);
         }
 
     protected:
@@ -58,9 +67,10 @@ class A
 int main()
 {
     RhIO::Root.load("rhio");
-    A a;
+    MyClass c;
 
     while (true) {
-        a.tick();
+        c.tick();
+        usleep(1000);
     }
 }
